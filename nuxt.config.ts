e994ae710779f3624552a3ba7f32001366829e4c
Wilson Huang <bussiness.weichen@gmail.com>
2023-12-07 14:02:46 +0800

    feature: update default config file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,4 +5,20 @@ export default defineNuxtConfig({
     typeCheck: true,
   },
   ssr: false,
+  // Pinia
+  pinia: {
+    autoImports: [
+      // automatically imports `defineStore`
+      // import { defineStore } from 'pinia'
+      "defineStore",
+      // import { defineStore as definePiniaStore } from 'pinia'
+      ["defineStore", "definePiniaStore"],
+    ],
+  },
+  runtimeConfig: {
+    kkboxGrantType: process.env.KKBOX_GRANT_TYPE,
+    kkboxClientID: process.env.KKBOX_CLIENT_ID,
+    kkboxClientSecret: process.env.KKBOX_CLIENT_SECRET,
+    baseURL: process.env.KKBOX_API
+  }
 });
