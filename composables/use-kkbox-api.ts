69fa0b62763727f663abd28cef4ed9d45e4504a0
Wilson Huang <bussiness.weichen@gmail.com>
2023-12-11 21:25:08 +0800

    feature: kkbox common api

---
+++ composables/use-kkbox-api.ts
@@ -0,0 +1,44 @@
+import type { UseFetchOptions } from "nuxt/app";
+import { useAuthStore } from "@/store/use-auth.store";
+
+const { accessToken, tokenType } = useAuthStore();
+const {
+  public: {
+    corsProxyAPI,
+    kkboxAPI,
+  }
+} = useRuntimeConfig();
+
+export const useKkboxApi = <ResT>(
+  path: string,
+  options?: UseFetchOptions<ResT>
+) => {
+  const loading = ref(false);
+  const error = ref();
+  const fetch = async () => {
+    loading.value = true;
+    try {
+      if (!path) return;
+
+      return await Promise.resolve(
+        useFetch(path, {
+          baseURL: `${corsProxyAPI}${kkboxAPI}`,
+          headers: {
+            Authorization: `${tokenType} ${accessToken}`
+          },
+          ...options
+        }),
+      );
+    } catch (e) {
+      error.value = e;
+      throw e;
+    } finally {
+      loading.value = false;
+    }
+  }; 
+  return {
+    loading: readonly(loading),
+    error: readonly(error),
+    fetch
+  };
+};
