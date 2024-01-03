92a0ed645c748d45699eb6c31b926ce5d53f0864
Wilson Huang <bussiness.weichen@gmail.com>
2024-01-03 14:24:03 +0800

    feature: create selector component

---
+++ components/selector/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="m-auto box-border w-[60vw] p-[12px]">
+    <ul class="flex w-full flex-wrap items-center justify-evenly gap-[24px]">
+      <li v-for="(option, index) in options" :key="index">
+        <a
+          class="text-center"
+          href=""
+          :title="option.title"
+          @click="
+            (e) => {
+              e.preventDefault();
+              emits('selected', option.id);
+            }
+          "
+        >
+          <img
+            :src="option.images[0].url"
+            :alt="option.title"
+            :srcset="
+              option.images.reduce((accumulator, current, index) => {
+                return accumulator + `${current.url} ${index + 1}x,`;
+              }, '')
+            "
+          />
+          <p class="m-auto max-w-[250px] truncate">
+            {{ option.title }}
+          </p>
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Category, Playlist } from '@/types/common.type';
+
+const emits = defineEmits<{
+  (e: 'selected', value?: string): void;
+}>();
+
+defineProps<{
+  options?: Category[] | Playlist[];
+}>();
+</script>
